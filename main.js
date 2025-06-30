3b8143e969b51426922f7fac187263d0f1099871
Farhat-fatima1 <fatimafarhat425@gmail.com>
2025-06-30 15:18:37 +0300

    First commit

--- main.js
+++ main.js
@@ -6,9 +6,3 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
-const menuIcon = document.getElementById("menu-icon");
-const navbar = document.getElementById("navbar");
-
-menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-});
